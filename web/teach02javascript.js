7829a4ee960e20c78c1996f707923896e8f1989c
Shaun Densley <twixx1987@gmail.com>
2018-09-25 21:16:33 -0700

    Updated java and html file for the third phase of the core requirements

--- web/teach02javascript.js
+++ web/teach02javascript.js
@@ -1,4 +1,11 @@
 // JavaScript source code
+
+// a function that displays the alert "You Clicked the button!"
 function clickAlert() {
     alert("You Clicked the button!");
+}
+
+// a function to change the color of the div
+function changeColor(inColor, one) {
+    one.setAttribute(color, inColor);
 }
