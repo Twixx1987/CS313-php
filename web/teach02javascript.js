22cf71c17cf53724693b39f0dc2ac50a1cdf012e
Shaun Densley <twixx1987@gmail.com>
2018-09-26 21:21:39 -0700

    Updated html, javascript, and css files to include stretch activities

--- web/teach02javascript.js
+++ web/teach02javascript.js
@@ -10,3 +10,19 @@ function changeColor() {
     var newColor = document.forms['frmOne'].elements[0].value;
     document.getElementById('one').style.color = newColor;
 }
+
+// a function to change the color of div two
+$(document).ready(
+    function () {
+        $("#btnChangeColor2").click(function () {
+            $("#two").css("color", $("input[name=inColor2]").val());
+        });
+    });
+
+// a function to change the visibility of div three
+$(document).ready(
+    function () {
+        $("#btnToggleDiv3").click(function () {
+            $("#three").fadeToggle("slow");
+        });
+    });
