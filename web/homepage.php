5e7f5026a8efbdadb3c1da1ab739f5ce995d751d
Shaun Densley <twixx1987@gmail.com>
2018-09-29 23:07:16 -0700

    spell checked content

--- web/homepage.php
+++ web/homepage.php
@@ -27,20 +27,20 @@
 	<div class="hero_content container">
 		<h2 class="hero_heading">Welcome intrepid heroes!<img src="Dolskuggar Ljuswarin.jpg" alt="Dolskuggar Ljuswarin" id="dolskuggar"></h2>
 		<p>Today I will be teaching you about another hero of legend, Dolskuggar Ljuswarin.</p>
-		<p>Dolskuggar commonly went by the nickname Dol. He was of great inportance during the shadow incursion in the new age. Dolskuggar was the one that warned the King of the impending invasion.</p>
-		<p>Dolskuggar Ljuswarin is of an uncommon race, the Shadow Elves. They came into existance after the Shadow War that ended the Silver Age. Dolskuggar's ancestors were among those that worked with 
-			the Great Sorcerer Alvar Bergljot. As you well know the Great Alvar is the one who discovered how to conjur light. His discovery turned the tide of the war against the shadows. But that is 
-			a story for another day. The Great Alvar taught a clan of elves how to conjur light. That clan became known as the Ljuswarin or Light Warriors. They are Dolskuggar's ancestors.</p>
-		<p>The Ljuswarin clan sacrificed themsleves to end the Shadow War. They entered the Shadow Realm to keep the Shadow creatures at bay. In doing so they locked themselves off of this world 
-			for millenia. Over those millenia their physical forms adapted to their new world. But the magic that started the Shadow War could not be reversed and over time the link this world had to 
+		<p>Dolskuggar commonly went by the nickname Dol. He was of great importance during the shadow incursion in the new age. Dolskuggar was the one that warned the King of the impending invasion.</p>
+		<p>Dolskuggar Ljuswarin is of an uncommon race, the Shadow Elves. They came into existence after the Shadow War that ended the Silver Age. Dolskuggar's ancestors were among those that worked with 
+			the Great Sorcerer Alvar Bergljot. As you well know the Great Alvar is the one who discovered how to conjure light. His discovery turned the tide of the war against the shadows. But that is 
+			a story for another day. The Great Alvar taught a clan of elves how to conjure light. That clan became known as the Ljuswarin or Light Warriors. They are Dolskuggar's ancestors.</p>
+		<p>The Ljuswarin clan sacrificed themselves to end the Shadow War. They entered the Shadow Realm to keep the Shadow creatures at bay. In doing so they locked themselves off of this world 
+			for millennia. Over those millennia their physical forms adapted to their new world. But the magic that started the Shadow War could not be reversed and over time the link this world had to 
 			the Shadow World grew allowing breaches to form and the Shadows began to return in dark places around the World.</p>
-		<p>It was at this time that Dolskuggar travelled from his home on the SHadow World to this world. He encountered a small band from the King's Initiative and together they prevented one 
+		<p>It was at this time that Dolskuggar travelled from his home on the Shadow World to this world. He encountered a small band from the King's Initiative and together they prevented one 
 			incursion point from being overrun by Shadows. He continued working with this group even as its membership changed. Eventually, they discovered a massive incursion point as well as a 
 			faithful friend from the Shadow World who sacrificed himself to warn the people of this world. Dolskuggar took this warning to the King's Initiative, who's leadership at first doubted Dolskuggar. 
 			But through luck, Dolskuggar managed to win the King's trust. With the King's trust he was able to mount a defense against the Shadows and prevent their invasion.</p>
 		<p>Dolskuggar received the highest Honors from the King and was even granted funding to find a way to break the spell that opened the connection between the worlds. He worked for decades 
-			as advisor to the King regarding Shadow Incursions. When he was not granting advice to the King he was diligently seeking a way to negate the spell. After twop decades of diligent research 
+			as advisor to the King regarding Shadow Incursions. When he was not granting advice to the King he was diligently seeking a way to negate the spell. After two decades of diligent research 
 			Dolskuggar found a way to Break the spell. He and some of his most diligent apprentices and associates finally broke the spell and thus saved this world from the threat of the Shadows.</p>
-	</div>
+</div>
 </body>
 </html>
