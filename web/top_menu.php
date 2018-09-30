65f209f7085aa5f29b162d3af3ca0c3f3fdc7996
Shaun Densley <twixx1987@gmail.com>
2018-09-29 22:48:11 -0700

    updates to the image alignment and navigation buttons

--- web/top_menu.php
+++ web/top_menu.php
@@ -1,10 +1,10 @@
 <?php
 echo '<ul class="nav menu-list">
 <li class="nav-item">
-<a class="nav-link menu-items" role="button" href="/homepage.php">Hobby</a>
+<a class="nav-link menu-items btn btn-secondary" role="button" href="/homepage.php">Hobby</a>
 </li>
 <li class="nav-item">
-<a class="nav-link menu-items" role="button" href="/assignments/assignments.php" class="menu">Assignments</a>
+<a class="nav-link menu-items btn btn-secondary" role="button" href="/assignments/assignments.php" class="menu">Assignments</a>
 </li>
 </ul>';
 ?>
