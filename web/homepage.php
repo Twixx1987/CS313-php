e2b84bd5faa0850d265cf9e2a19e880087b77968
Shaun Densley <twixx1987@gmail.com>
2018-09-29 19:45:29 -0700

    Updated the content of the Homepage file to include a bio about Dolskuggar Ljuswarin

--- web/homepage.php
+++ web/homepage.php
@@ -17,10 +17,28 @@
     <script src="homepage.js"></script>
 
     <!-- Page title -->
-    <title>CS313 - Shaun Densley</title>
+    <title>CS313 - Welcome Heroes!</title>
 </head>
 <body>
-    <h1>CS313 - Shaun Densley</h1>
-
+    <h1>CS313 - Welcome Heroes!</h1>
+	<?php include 'top_menu.php' ?>
+	<div>
+	<p>Welcome intrepid heroes!</p>
+	<p>Today I will be teaching you about another hero of legend, Dolskuggar Ljuswarin.</p>
+	<p>Dolskuggar commonly went by the nickname Dol. He was of great inportance during the shadow incursion in the new age. Dol was the one that warned the King of the impending invasion.</p>
+	<p>Dolskuggar Ljuswarin is of an uncommon race, the Shadow Elves. They came into existance after the Shadow War that ended the Silver Age. Dol's ancestors were among those that worked with 
+	the Great Sorcerer Alvar Bergljot. As you well know the Great Alvar is the one who discovered how to conjur light. His discovery turned the tide of the war against the shadows. But that is 
+	a story for another day. The Great Alvar taught a clan of elves how to conjur light. That clan became known as the Ljuswarin or Light Warriors. They are Dolskuggar's ancestors.</p>
+	<p>The Ljuswarin clan sacrificed themsleves to end the Shadow War. They entered the Shadow Realm to keep the Shadow creatures at bay. In doing so they locked themselves off of this world 
+	for millenia. Over those millenia their physical forms adapted to their new world. But the magic that started the Shadow War could not be reversed and over time the link this world had to 
+	the Shadow World grew allowing breaches to form and the Shadows began to return in dark places around the World.</p>
+	<p>It was at this time that Dolskuggar travelled from his home on the SHadow World to this world. He encountered a small band from the King's Initiative and together they prevented one 
+	incursion point from being overrun by Shadows. He continued working with this group even as its membership changed. Eventually, they discovered a massive incursion point as well as a 
+	faithful friend from the Shadow World who sacrificed himself to warn the people of this world. Dolskuggar took this warning to the King's Initiative, who's leadership at first doubted Dolskuggar. 
+	But through luck, Dol managed to win the King's trust. With the King's trust he was able to mount a defense against the Shadows and prevent their invasion.</p>
+	<p>Dolskuggar received the highest Honors from the King and was even granted funding to find a way to break the spell that opened the connection between the worlds. He worked for decades 
+	as advisor to the King regarding Shadow Incursions. When he was not granting advice to the King he was diligently seeking a way to negate the spell. After twop decades of diligent research 
+	Dolskuggar found a way to Break the spell. He and some of his most diligent apprentices and associates finally broke the spell and thus saved this world from the threat of the Shadows.</p>
+	</div>
 </body>
 </html>
