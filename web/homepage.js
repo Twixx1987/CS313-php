9c713142571f0e6f88143654ca2159d0770f4796
Shaun Densley <twixx1987@gmail.com>
2018-09-29 21:55:49 -0700

    update formatting for image, add animation to image

--- web/homepage.js
+++ web/homepage.js
@@ -1,28 +1,13 @@
 // JavaScript source code
 
-// a function that displays the alert "You Clicked the button!"
-function clickAlert() {
-    alert("You Clicked the button!");
-}
-
-// a function to change the color of the div
-function changeColor() {
-    var newColor = document.forms['frmOne'].elements[0].value;
-    document.getElementById('one').style.color = newColor;
-}
-
-// a function to change the color of div two
+// a function to zoom in on the image
 $(document).ready(
     function () {
-        $("#btnChangeColor2").click(function () {
-            $("#two").css("color", $("input[name=inColor2]").val());
-        });
-    });
-
-// a function to change the visibility of div three
-$(document).ready(
-    function () {
-        $("#btnToggleDiv3").click(function () {
-            $("#three").fadeToggle("slow");
+        $("#dolskuggar").mouseenter(function () {
+            $("#dolskuggar").animate({
+                right: '250px',
+                width: '400px',
+                height: '500px'
+            }, "slow");
         });
     });
