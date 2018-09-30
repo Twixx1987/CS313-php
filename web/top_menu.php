56cc1216005d33c575d5780bd6920183ef73d156
Shaun Densley <twixx1987@gmail.com>
2018-09-29 20:21:54 -0700

    Update to formatting for assignments page, homepage, top_menu.php, add Dolskugarr_Ljuswaring.jpg file

--- web/top_menu.php
+++ web/top_menu.php
@@ -1,4 +1,4 @@
 <?php
-echo '<a href="/homepage.php">Hobby</a>
-<a href="/assignments/assignments.php">Assignments</a>';
+echo '<div class="menu><a href="/homepage.php">Hobby</a></div>
+<div class="menu><a href="/assignments/assignments.php" class="menu">Assignments</a></div>';
 ?>
