2765177c1fa6cf65ddbedaf6365f3b1db3445aba
Shaun Densley <twixx1987@gmail.com>
2018-09-29 22:26:28 -0700

    updates to nav and image

--- web/assignments/assignments.php
+++ web/assignments/assignments.php
@@ -21,9 +21,9 @@
 </head>
 <body>
     <h1 class="pagetitle">CS313 Assignments - Shaun Densley</h1>
-	<nav class="menu">
+	<div class="menu">
 		<?php include '../top_menu.php'; ?>
-	</nav>
+	</div>
 	<div class="construction bg-warning">
 	  <div class="card-header">&#x26A0 Content Under Construction! &#x26A0</div>
 	  <div class="card-body">
--- web/top_menu.php
+++ web/top_menu.php
@@ -1,12 +1,10 @@
 <?php
-echo '<div class="collapse navbar-collapse">
-<ul class="navbar-nav mr-auto">
+echo '<ul class="nav">
 <li class="nav-item">
 <a class="nav-link" href="/homepage.php">Hobby</a>
 </li>
 <li class="nav-item">
 <a class="nav-link" href="/assignments/assignments.php" class="menu">Assignments</a>
 </li>
-</ul>
-</div>';
+</ul>';
 ?>
