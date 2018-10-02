ded20d5b7cc0dbb5d19b3d96a6de409aa0383fcf
Shaun Densley <twixx1987@gmail.com>
2018-10-02 16:30:46 -0700

    update menu paths

--- web/top_menu.php
+++ web/top_menu.php
@@ -3,8 +3,12 @@ echo '<ul class="nav menu-list">
 <li class="nav-item">
 <a class="nav-link menu-items btn btn-secondary" role="button" href="/homepage.php">Hobby</a>
 </li>
-<li class="nav-item">
-<a class="nav-link menu-items btn btn-secondary" role="button" href="/assignments/assignments.php" class="menu">Assignments</a>
+<li class="nav-item dropdown">
+<a class="nav-link menu-items btn btn-secondary dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+<div class="dropdown-menu" href="/assignments/assignments.php">Assignments
+  <a class=dropdown-item menu" role="button" href="/assignments/week02/teach02.html">Teach 02</a>
+  <a class=dropdown-item menu" role="button" href="/assignments/week03/teach03.php">Teach 03</a>
+</div>
 </li>
 </ul>';
 ?>
