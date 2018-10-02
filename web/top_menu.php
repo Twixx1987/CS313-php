7ac34919fdc95d521cad4fb6582183c271cd5d35
Shaun Densley <twixx1987@gmail.com>
2018-10-02 16:54:08 -0700

    update menu format

--- web/top_menu.php
+++ web/top_menu.php
@@ -1,14 +1,14 @@
 <?php
 echo '<ul class="nav nav-pills flex-column flex-sm-row menu-list">
 <li class="nav-item flex-sm-fill text-sm-center">
-<a class="nav-link menu-items flex-sm-fill text-sm-center btn btn-secondary" role="button" href="/homepage.php">Hobby</a>
+<a class="nav-link menu-items flex-sm-fill text-sm-center" href="/homepage.php">Hobby</a>
 </li>
 <li class="nav-item dropdown flex-sm-fill text-sm-center">
-<a class="nav-link menu-items flex-sm-fill text-sm-center btn btn-secondary dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Assignments</a>
+<a class="nav-link menu-items flex-sm-fill text-sm-center dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Assignments</a>
 <div class="dropdown-menu">
-  <a class="dropdown-item flex-sm-fill text-sm-center menu" role="button" href="/assignments/assignments.php">Assignments Home</a>
-  <a class="dropdown-item flex-sm-fill text-sm-center menu" role="button" href="/assignments/week02/teach02.php">Teach 02</a>
-  <a class="dropdown-item flex-sm-fill text-sm-center menu" role="button" href="/assignments/week03/teach03.php">Teach 03</a>
+  <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/assignments.php">Assignments Home</a>
+  <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/week02/teach02.php">Teach 02</a>
+  <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/week03/teach03.php">Teach 03</a>
 </div>
 </li>
 </ul>';
