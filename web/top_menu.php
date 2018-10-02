be18ecd30ec269f86f67732a04a4944c5aec6cdd
Shaun Densley <twixx1987@gmail.com>
2018-10-02 16:39:00 -0700

    change teach 02 to a php file instead of html file

--- web/top_menu.php
+++ web/top_menu.php
@@ -4,8 +4,8 @@ echo '<ul class="nav menu-list">
 <a class="nav-link menu-items btn btn-secondary" role="button" href="/homepage.php">Hobby</a>
 </li>
 <li class="nav-item dropdown">
-<a class="nav-link menu-items btn btn-secondary dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-<div class="dropdown-menu" href="/assignments/assignments.php">Assignments
+<a class="nav-link menu-items btn btn-secondary dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="/assignments/assignments.php">Assignments</a>
+<div class="dropdown-menu">
   <a class=dropdown-item menu" role="button" href="/assignments/week02/teach02.html">Teach 02</a>
   <a class=dropdown-item menu" role="button" href="/assignments/week03/teach03.php">Teach 03</a>
 </div>
