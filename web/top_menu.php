df9b0c81bdd0cedc1f6c08408196b85601393d01
Shaun Densley <twixx1987@gmail.com>
2018-10-02 17:07:00 -0700

    update dropdown formatting

--- web/top_menu.php
+++ web/top_menu.php
@@ -5,7 +5,7 @@ echo '<ul class="nav nav-pills flex-column flex-sm-row menu-list">
 </li>
 <li class="nav-item dropdown flex-sm-fill text-sm-center">
 <a class="nav-link menu-items flex-sm-fill text-sm-center dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Assignments</a>
-<div class="dropdown-menu flex-sm-fill text-sm-center">
+<div class="dropdown-menu flex-column flex-sm-row">
   <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/assignments.php">Assignments Home</a>
   <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/week02/teach02.php">Teach 02</a>
   <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/week03/teach03.php">Teach 03</a>
