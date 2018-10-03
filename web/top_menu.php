6021771cb08cfe2d284a4bc8224cc4d47e2b2301
Shaun Densley <twixx1987@gmail.com>
2018-10-02 20:32:47 -0700

    update teach 03 list layout

--- web/top_menu.php
+++ web/top_menu.php
@@ -5,7 +5,7 @@ echo '<ul class="nav flex-column flex-sm-row menu-list btn btn-secondary">
         </li>
         <li class="nav-item dropdown flex-sm-fill text-sm-center">
           <a class="nav-link menu-items flex-sm-fill text-sm-center dropdown-toggle btn btn-secondary" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Assignments</a>
-          <div class="dropdown-menu mx-auto">
+          <div class="dropdown-menu">
             <a class="dropdown-item menu btn btn-secondary" role="button" href="/assignments/assignments.php">Assignments Home</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item menu btn btn-secondary" role="button" href="/assignments/week02/teach02.php">Teach 02</a>
