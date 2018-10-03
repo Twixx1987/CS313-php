cb1a87af405d17556d845d00e46743a5dd04028e
Shaun Densley <twixx1987@gmail.com>
2018-10-02 17:31:47 -0700

    update teach 03 output page

--- web/assignments/week03/teach03form.php
+++ web/assignments/week03/teach03form.php
@@ -47,13 +47,13 @@ function cleanInputs($data) {
 	</div>
 	<div class="container">
 		<p>Name: <?php echo $name;?></p>
-		<p>Email: <?php echo "<a href=/"mailto:$email/">$email</a>";?></p>
+		<p>Email: <a href="mailto:<?php echo $email;?>"> <?php echo $email;?></a></p>
 		<p>Major: <?php echo $major;?></p>
 		<p>Comments: <?php echo $comments;?></p>
 		<p>Continents Visited: <?php 
 									foreach ($continent as $place) {
-									echo "$place <br>";
-								}
+										echo "$place <br>";
+									}
 								?>
 								</p>
 	</div>
