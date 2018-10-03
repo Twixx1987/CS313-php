95bbc26c257657afe1a4a7f1f53ea53e0344a4de
Shaun Densley <twixx1987@gmail.com>
2018-10-02 19:55:36 -0700

    update nav layout

--- web/top_menu.php
+++ web/top_menu.php
@@ -1,15 +1,16 @@
 <?php
-echo '<ul class="nav nav-pills flex-column flex-sm-row menu-list">
-<li class="nav-item flex-sm-fill text-sm-center">
-<a class="nav-link menu-items flex-sm-fill text-sm-center" href="/homepage.php">Hobby</a>
-</li>
-<li class="nav-item dropdown flex-sm-fill text-sm-center">
-<a class="nav-link menu-items flex-sm-fill text-sm-center dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Assignments</a>
-<div class="dropdown-menu flex-column flex-sm-row">
-  <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/assignments.php">Assignments Home</a>
-  <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/week02/teach02.php">Teach 02</a>
-  <a class="dropdown-item flex-sm-fill text-sm-center menu" href="/assignments/week03/teach03.php">Teach 03</a>
-</div>
-</li>
-</ul>';
+echo '<ul class="nav flex-column flex-sm-row menu-list btn btn-primary">
+        <li class="nav-item flex-sm-fill text-sm-center">
+          <a class="nav-link menu-items flex-sm-fill text-sm-center btn btn-primary" role="button" href="/homepage.php">Hobby</a>
+        </li>
+        <li class="nav-item dropdown flex-sm-fill text-sm-center">
+          <a class="nav-link menu-items flex-sm-fill text-sm-center dropdown-toggle btn btn-primary" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Assignments</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item menu btn btn-primary" role="button" href="/assignments/assignments.php">Assignments Home</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item menu btn btn-primary" role="button" href="/assignments/week02/teach02.php">Teach 02</a>
+            <a class="dropdown-item menu btn btn-primary" role="button" href="/assignments/week03/teach03.php">Teach 03</a>
+          </div>
+        </li>
+      </ul>';
 ?>
