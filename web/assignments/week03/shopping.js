491f6968ab61ea10eadb2fcd934638ef16181de6
Shaun Densley <twixx1987@gmail.com>
2018-10-06 01:42:53 -0700

    update cart layout

--- web/assignments/week03/shopping.js
+++ web/assignments/week03/shopping.js
@@ -1,6 +1,6 @@
 // javascript function to add additional menu to shopping pages
 $(document).ready(function () {
     $("#totals").empty();
-    $("#totals").append("<h3>Your Shopping Cart is currently Empty!</h3><br/><h4>Try navigating to the "
-    + "<a href='browse.php'>Catalog</a> to select something to buy.</h4>");
+    $("#totals").append("<div class='col'><h3>Your Shopping Cart is currently Empty!</h3><br/><h4>Try navigating to the "
+    + "<a href='browse.php'>Catalog</a> to select something to buy.</h4></div>");
 });
