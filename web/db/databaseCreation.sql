c60fc03621dc98665c199d3b7e2a7a777b1bef91
Shaun Densley <twixx1987@gmail.com>
2018-10-13 18:49:22 -0700

    Added additional sql command file for version aandcharacter insertion.

--- web/db/databaseCreation.sql
+++ web/db/databaseCreation.sql
@@ -1,33 +1,36 @@
-CREATE TABLE characters (
+CREATE TABLE rdi_version (
+	version_id serial PRIMARY KEY,
+	version_name varchar(80)
+);
+
+CREATE TABLE rdi_characters (
 	character_id serial PRIMARY KEY,
-	version_id integer NOT NULL REFERENCES version (version_id),
+	version_id integer NOT NULL REFERENCES rdi_version (version_id),
 	character_name varchar(80),
 	race varchar(80),
+	class varchar(80),
 	good text,
 	bad text,
+	worse text
 );
 
-CREATE TABLE version (
-	version_id serial PRIMARY KEY,
-	version_name varchar(80),
-);
-
-CREATE TABLE player (
-	player_id serial PRIMARY KEY,
-	game_id integer REFERENCES game (game_id),
-	user_id integer REFERENCES user (user_id),
-	character_id integer REFERENCES character (character_id),
-	placement integer,
-);
-
-CREATE TABLE game (
+CREATE TABLE rdi_game (
 	game_id serial PRIMARY KEY,
-	player_count integer NOT NULL CHECK (player_count > 1),
+	player_count integer NOT NULL CHECK (player_count > 1)
 );
 
-CREATE TABLE user (
+CREATE TABLE rdi_user (
 	user_id serial PRIMARY KEY,
 	user_name varchar(80) UNIQUE NOT NULL,
 	email text UNIQUE NOT NULL,
-	password varchar(80) NOT NULL,
+	password varchar(80) NOT NULL
 );
+
+CREATE TABLE rdi_player (
+	player_id serial PRIMARY KEY,
+	game_id integer REFERENCES rdi_game (game_id),
+	user_id integer REFERENCES rdi_user (user_id),
+	character_id integer REFERENCES rdi_characters (character_id),
+	placement integer
+);
+
