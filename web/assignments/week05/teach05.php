1d83018677dcd0df9e8439e68dbb4e6779570e76
Shaun Densley <twixx1987@gmail.com>
2018-10-16 17:55:04 -0700

    Teach 05 assignment

--- web/assignments/week05/teach05.php
+++ web/assignments/week05/teach05.php
@@ -1,3 +1,21 @@
+<?php
+// set variables for the inputs
+$book = "";
+
+// get the input data
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+	$book = cleanInputs($_POST["book"]);
+}
+
+// a function to clean the data
+function cleanInputs($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+?>
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
@@ -10,6 +28,10 @@
 </head>
 <body>
 	<h1>Scripture Resources</h1>
+	<form id="search" name="search" method="post" action="teach05.php">
+		<label for="book">Enter the name of a book to search for content from it.</label><br />
+		<input type="text" name="book" id="book" placeholder="Book"></input>
+	</form>
 	<?php
 		try
 		{
@@ -33,13 +55,15 @@
 		  die();
 		}
 
-		$statement = $db->query('SELECT book, chapter, verse, content FROM scriptures');
-		while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-		{
-		  echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong>';
-		  echo ' - "' . $row['content'] . '"<br/>'; 
+		if($_SERVER["REQUEST_METHOD"]=="POST") {
+	
+			$statement = $db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book=' . $book);
+			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+			{
+			  echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong>';
+			  echo ' - "' . $row['content'] . '"<br/>'; 
+			}
 		}
-
 	?>
 </body>
 </html>
