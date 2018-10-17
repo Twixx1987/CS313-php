00f612ab91272695d09f18fc7a6691ff3810c47f
Shaun Densley <twixx1987@gmail.com>
2018-10-16 18:00:36 -0700

    Teach 05 assignment

--- web/assignments/week05/teach05.php
+++ web/assignments/week05/teach05.php
@@ -30,7 +30,7 @@ function cleanInputs($data) {
 	<h1>Scripture Resources</h1>
 	<form id="search" name="search" method="post" action="teach05.php">
 		<label for="book">Enter the name of a book to search for content from it.</label><br />
-		<input type="text" name="book" id="book" placeholder="Book"><br/>
+		<input type="text" name="book" id="book" placeholder="Book" <?php if ($_SERVER["REQUEST_METHOD"]=="POST") echo 'value="' . $book .'"';?>><br/>
 		<input type="submit" value="Submit" name="submit">
 	</form>
 	<?php
