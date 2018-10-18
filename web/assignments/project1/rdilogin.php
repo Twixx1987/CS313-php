21d9a38bf4473efffe67794fccc9306c0d842864
Shaun Densley <twixx1987@gmail.com>
2018-10-18 12:59:31 -0700

    Initial upload of RDI project files

---
+++ web/assignments/project1/rdilogin.php
@@ -0,0 +1,40 @@
+<?php include 'rdidbconnect.php'; ?>
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS and Custom Stylesheet -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="rdi.css" />
+    
+    <!-- Required JavaScript and Custom JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="../assignments.js"></script>
+
+    <!-- Page title -->
+    <title>RDI Login</title>
+</head>
+<body>
+	<h1 class="pagetitle container">RDI Login</h1>
+	<div class="menu container">
+		<?php include 'rdimenu.php'; ?>
+	</div>
+	<div class="container">
+		<form id="login" name="login" method="post" action="">
+			<label for="username">Username:</label><br/>
+			<input type="text" name="username"><br/>
+			<label for="password">Password:</label><br/>
+			<input type="password" name="password"><br/>
+			<input type="submit" name="login" value="Login">
+			<button id="signup" onclick="signup">Sign Up</button>
+			<input type="checkbox" name="signupcheck" id="signupcheck">
+		</form>
+	</div>
+</body>
+</html>
