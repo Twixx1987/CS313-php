fc185b3ac14584804f6f82d72d4413d4693f5568
Shaun Densley <twixx1987@gmail.com>
2018-10-18 20:51:04 -0700

    Initial upload of RDI project files

--- web/assignments/project1/rdilogin.php
+++ web/assignments/project1/rdilogin.php
@@ -5,12 +5,46 @@ session_start();
 // include the DB connection
 include 'rdidbconnect.php'; 
 
-// initialize the username variable
-$username = "";
+// create a variable to store a login error if needed
+$error = "";
 
 // check for post login button
 if (isset($_POST['username'])) {
-	$username = $_POST['username'];
+	// get the username and password and clean the inputs
+	$username = cleanInputs($_POST['username']);
+	$password = cleanInputs($_POST['password']);
+
+	// query the database for the username and password
+	$statement = $db->prepare('SELECT username, password, user_id FROM rdi_user WHERE username=:username');
+	$statement->execute(array(':username' => $username));
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		// check to see if the username/password combo match the DB
+		if ($row['username'] == $username && $row['password'] == $password) {
+			// set the user_id session variable
+			$_SESSION['user_id'] = $row['user_id'];
+
+			// clean the output buffer
+			ob_clean();
+
+			// redirect to the home page based on code from https://www.bing.com/videos/search?q=how+to+redirect+to+another+page+using+php&view=detail&mid=09FEDBEAEB640A5D76BE09FEDBEAEB640A5D76BE&FORM=VIRE
+			header('Location: http://' . $_SERVER['HTTP_HOST'] . '/rdihome.php', true, 303);
+
+			// terminate php script upon redirect
+			exit();
+		}
+	}
+
+	// if no match was found populate an error message
+	$error = "Username nad Password not found. Please try again.";
+}
+
+// a function to clean the data
+function cleanInputs($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
 }
 ?>
 <!DOCTYPE html>
@@ -40,9 +74,12 @@ if (isset($_POST['username'])) {
 		<?php include 'rdimenu.php'; ?>
 	</div>
 	<div class="container">
+		<div class="container">
+			<?php echo $error;?>
+		</div>
 		<form id="login" name="login" method="post" action="rdilogin.php">
 			<label for="username">Username:</label><br/>
-			<input type="text" name="username" value="<?php echo $username;?>"<br/>
+			<input type="text" name="username"><br/>
 			<label for="password">Password:</label><br/>
 			<input type="password" name="password"><br/>
 			<input type="submit" name="login" value="Login">
