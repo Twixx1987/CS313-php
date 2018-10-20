d9f22a723fdf909313bf6394e28877e92c39005e
Shaun Densley <twixx1987@gmail.com>
2018-10-19 21:03:02 -0700

    begin adding javascript functions

---
+++ web/assignments/project1/rdi.js
@@ -0,0 +1,8 @@
+// a function to check all versions
+$(document).ready(function(){
+    // select all was clicked
+   $('#selectAll').click(function() {
+       $(':checkbox').attr("checked", "true");
+   });
+});
+
