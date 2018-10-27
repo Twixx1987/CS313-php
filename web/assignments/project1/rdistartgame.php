e36cd75165f5db669c6a2f5d9bc62c178564790f
Shaun Densley <twixx1987@gmail.com>
2018-10-27 00:46:43 -0700

    begin creating php file to process the game start

--- web/assignments/project1/rdistartgame.php
+++ web/assignments/project1/rdistartgame.php
@@ -9,42 +9,45 @@
     $player_count = $_POST["playerCount"];
 
     // create the prepared query to create the game_id
-    $statement = $db->prepare('INSERT INTO rdi_game (player_count) VALUES (:player_count)');
-    $statement->execute(array(':player_count' => $player_count));
+    $dbInsert = $db->prepare('INSERT INTO rdi_game (player_count) VALUES (:player_count)');
+    $dbInsert->execute(array(':player_count' => $player_count));
 
     // get the game id
     $game_id = $db->lastInsertId('rdi_game_game_id_seq');
 
-    // add
+    // get the characters array from the session
+    $characters = $_SESSION["characters"];
 
+    // create a random keys array
+    $rand_keys = array_rand($characters, $player_count);
 
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        // is the game ID is valid
-        if ($row["game_id"] == $game_id && $row["game_open"] && $row["player_count"] > $row["joined_count"]) {
-            // get a character from the game_character table
-            $statement2 = $db->prepare('SELECT character_id FROM rdi_game_characters WHERE game_id=:game_id LIMIT 1');
-            $statement2->execute(array(':game_id' => $game_id));
-            while ($row2 = $statement2->fetch(PDO::FETCH_ASSOC)) {
-                $character_id = $row2["character_id"];
-            }
+    // add random characters equal to the number of anticipated players
+    for ($count = 1; $count < $player_count; $count++) {
+        // get the character id
+        $character_id = $characters[$rand_keys[$count]];
 
-            // insert the player into the game
-            $dbInsert = $db->prepare('INSERT INTO rdi_player (game_id, user_id, character_id) VALUES (:game_id, :user_id, :character_id)');
-            $dbInsert->execute(array(':game_id' => $game_id, ':user_id' => $user_id, ':character_id' => $character_id));
+        // parse out the 'character_' part of the character id
+        $character_id = str_replace("character_","", $character_id);
 
-            // remove that character from the game_characters table
-            $dbDelete = $db->prepare('DELETE FROM rdi_game_characters WHERE game_id=:game_id AND character_id=:character_id)');
-            $dbDelete->execute(array(':game_id' => $game_id, ':character_id' => $character_id));
-
-            // output a joined game message
-            ?>
-            <p class="container">SUCCESS: You have joined Game #<?php echo $game_id; ?></p>
-            <?php
-        } else {
-            // output an error message
-            ?>
-            <p class="container error">ERROR: Invalid Game ID <?php echo $game_id; ?></p>
-            <?php
-        }
+        // create the prepared query to add the game characters
+        $dbInsert2 = $db->prepare('INSERT INTO rdi_game_characters (game_id, character_id) VALUES (:game_id, :character_id)');
+        $dbInsert2->execute(array(':game_id' => $game_id, ':character_id' => $character_id));
     }
+
+    // get the first character from the array
+    $character_id = $characters[$rand_keys[0]];
+
+    // parse out the 'character_' part of the character id
+    $character_id = str_replace("character_","", $character_id);
+
+    // insert creator into the game
+    $dbInsert3 = $db->prepare('INSERT INTO rdi_player (game_id, user_id, character_id) VALUES (:game_id, :user_id, :character_id)');
+    $dbInsert3->execute(array(':game_id' => $game_id, ':user_id' => $user_id, ':character_id' => $character_id));
+
+    // remove that character from the game_characters table
+    $dbDelete = $db->prepare('DELETE FROM rdi_game_characters WHERE game_id=:game_id AND character_id=:character_id)');
+    $dbDelete->execute(array(':game_id' => $game_id, ':character_id' => $character_id));
+
+// output a joined game message
 ?>
+<p class="container">SUCCESS: You have created Game #<?php echo $game_id; ?></p>
