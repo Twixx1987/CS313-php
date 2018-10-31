38cfcf7d68efd47a825741cdc8b522f6cfea3321
Shaun Densley <twixx1987@gmail.com>
2018-10-31 11:50:18 -0700

    correct spelling of SlugFest on footer

--- web/assignments/project1/rdirightsfooter.php
+++ web/assignments/project1/rdirightsfooter.php
@@ -1,2 +1,4 @@
-<p>The Red Dragon Inn and The Red Dragon Inn characters are trademarks or registered trademarks of Slugfest Games Incorporated. All rights reserved.</p>
-<p>For more information about The Red Dragon Inn see <a href="http://slugfestgames.com/games/rdi/">Slugfest Games</a> website.</p>
+<p>The Red Dragon Inn and The Red Dragon Inn characters are trademarks or
+    registered trademarks of SlugFest Games Incorporated. All rights reserved.</p>
+<p>For more information about The Red Dragon Inn see
+    <a href="http://slugfestgames.com/games/rdi/">SlugFfest Games</a> website.</p>
