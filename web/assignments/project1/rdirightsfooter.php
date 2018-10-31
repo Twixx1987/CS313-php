82db469694b5fea04ab7d2294b6417927b904986
Shaun Densley <twixx1987@gmail.com>
2018-10-31 15:20:04 -0700

    Added verbage that SlugFest specified.

--- web/assignments/project1/rdirightsfooter.php
+++ web/assignments/project1/rdirightsfooter.php
@@ -1,4 +1,5 @@
 <p>The Red Dragon Inn and The Red Dragon Inn characters are trademarks or
     registered trademarks of SlugFest Games Incorporated. All rights reserved.</p>
+<p>Images copyright SlugFest Games, used with permission.</p>
 <p>For more information about The Red Dragon Inn see
     <a href="http://slugfestgames.com/games/rdi/">SlugFfest Games</a> website.</p>
