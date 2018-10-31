ba409a830bd76af374eb77f5a898b48127ad1dc4
Shaun Densley <twixx1987@gmail.com>
2018-10-31 16:41:11 -0700

    debugging for version table

--- web/assignments/project1/rdisettings.php
+++ web/assignments/project1/rdisettings.php
@@ -77,11 +77,11 @@
                         echo ' $count % 3 : ';
                         echo $count % 3;
                         echo ' $count % 3 == 0 : ';
-                        echo $count % 3 == 0;
+                        echo $count % 3 === 0;
                         echo '<br>';
 
 					    // every third cell start a new row
-					    if ($count % 3 == 0):
+					    if ($count % 3 === 0):
 				?>
                 <tr>
                 <?php endif; ?>
@@ -89,7 +89,7 @@
                         <input type="checkbox" class="versionSelector version_<?php echo $row['version_id']; ?>" id="version_<?php echo $row['version_id']; ?>"
                                    name="version_<?php echo $row['version_id']; ?>" value="version_<?php echo $row['version_id']; ?>">
                     </td>
-                <?php if ($count % 3 == 0): ?>
+                <?php if ($count % 3 === 0): ?>
                 </tr>
                 <?php endif; ?>
 				<?php
