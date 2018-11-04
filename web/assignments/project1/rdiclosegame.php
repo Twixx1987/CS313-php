049f82d6d171c04696214138b8b7fa047075b8eb
Shaun Densley <twixx1987@gmail.com>
2018-11-03 23:19:20 -0700

    delete and remaining game_characters from said table

--- web/assignments/project1/rdiclosegame.php
+++ web/assignments/project1/rdiclosegame.php
@@ -30,9 +30,9 @@
         $dbUpdate->execute(array(':player_count' => $result[0]['player_count'], ':game_id' => $game_id, ':host_user' => $user_id));
 
         // delete any remaining characters from the game_characters table
-        $deleteQuery = "DELETE FROM rdi_game_charaters WHERE game_id = :game_id";
+        $deleteQuery = "DELETE FROM rdi_game_characters WHERE game_id=:game_id";
         $dbDelete = $db->prepare($deleteQuery);
-        $dbDelete->execute(array(':game_id' => $game_id));
+        $dbDelete->execute(array(":game_id" => $game_id));
     }  else {
         // clean the output buffer
         ob_clean();
